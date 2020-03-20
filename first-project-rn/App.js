9d3577b17d560449b3e8fee1afcb64106684dce0
lopeselio <lopeselio@gmail.com>
2020-03-20 15:22:42 +0530

    fixed errors

--- first-project-rn/App.js
+++ first-project-rn/App.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default function App () {
   const [outputText, setOutputText] = useState('Open up App.js to start working on your App!')
@@ -7,9 +7,9 @@ export default function App () {
   return (
     <View style={styles.container}>
       <Text>{outputText}</Text>
-      <Button title='Change Text' onPress={() => setOutputText('The text changed!')}>
+      <Button title='Change Text' onPress={() => setOutputText('The text changed!')} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({

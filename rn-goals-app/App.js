ed2f13a585448791d3604c793a25128d58e183cf
lopeselio <lopeselio@gmail.com>
2020-03-21 16:32:31 +0530

    added stylesheet

--- rn-goals-app/App.js
+++ rn-goals-app/App.js
@@ -1,19 +1,48 @@
 import React from 'react'
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import { Text, View } from 'react-native'
 
 export default function App () {
   return (
-    <View style={{ padding: 50 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-        <TextInput
-          placeholder='Course Goal'
-          style={{ width: '80%', borderColor: 'black', borderWidth: 5, padding: 10 }}
-        />
-        <Button title='ADD' />
+    <View
+      style={{
+        padding: 50,
+        flexDirection: 'row',
+        width: '80%',
+        height: 300,
+        justifyContent: 'space-around',
+        alignItems: 'stretch'
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'red',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>1</Text>
       </View>
-      <View />
+      <View
+        style={{
+          backgroundColor: 'blue',
+          flex: 2,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: 'green',
+
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Text>3</Text>
+      </View>
     </View>
   )
 }
-
-const styles = StyleSheet.create({})

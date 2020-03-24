071074b0b7b19040e48484dc5fbe27ece77cb7ad
lopeselio <lopeselio@gmail.com>
2020-03-24 17:32:09 +0530

    added components

--- rn-goals-app/App.js
+++ rn-goals-app/App.js
@@ -1,29 +1,31 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native'
+import {
+  StyleSheet,
+  View,
+  FlatList
+} from 'react-native'
+
+import GoalItem from './components/GoalItem'
+import GoalInput from './components/GoalInput'
 
 export default function App () {
-  const [enteredGoal, setEnteredGoal] = useState('')
   const [courseGoals, setCourseGoals] = useState([])
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText)
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }
+    ])
   }
 
-  const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...courseGoals, { id: Math.random().toString(), value: enteredGoal }])
-  }
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder='Enter your Course GOAL!'
-          style={styles.Input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button title='ADD' onPress={addGoalHandler} />
-      </View>
-      <FlatList keyExtractor={(item, index) => item.id} data={courseGoals} renderItem={itemData => (<View style={styles.listItem}><Text>{itemData.item.value}</Text></View>)} />
+      <GoalInput onAddGoal={addGoalHandler} />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => <GoalItem title={itemData.item.value} />}
+      />
     </View>
   )
 }
@@ -31,23 +33,5 @@ export default function App () {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  Input: {
-    width: '80%',
-    borderColor: 'black',
-    borderWidth: 5,
-    padding: 10
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: '#000000',
-    borderWidth: 1
   }
 })

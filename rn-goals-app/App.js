061ed70410b77f6da44ae35e8e40560c17bc8436
lopeselio <lopeselio@gmail.com>
2020-03-24 15:44:39 +0530

    added scrollview

--- rn-goals-app/App.js
+++ rn-goals-app/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native'
 
 export default function App () {
   const [enteredGoal, setEnteredGoal] = useState('')
@@ -23,9 +23,9 @@ export default function App () {
         />
         <Button title='ADD' onPress={addGoalHandler} />
       </View>
-      <View>
+      <ScrollView>
         {courseGoals.map((goal) => <View key={goal} style={styles.listItem}><Text>{goal}</Text></View>)}
-      </View>
+      </ScrollView>
     </View>
   )
 }

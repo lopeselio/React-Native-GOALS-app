37b0fb3ebdad98544abbebe478f7270cdd01fbd3
lopeselio <lopeselio@gmail.com>
2020-03-25 09:48:39 +0530

    added cancel addition button

--- rn-goals-app/components/GoalInput.js
+++ rn-goals-app/components/GoalInput.js
@@ -8,6 +8,11 @@ const GoalInput = props => {
     setEnteredGoal(enteredText)
   }
 
+  const addGoalHandler = () => {
+    props.onAddGoal(enteredGoal)
+    setEnteredGoal('')
+  }
+
   return (
     <Modal visible={props.visible} animationType='slide'>
       <View style={styles.inputContainer}>
@@ -17,7 +22,8 @@ const GoalInput = props => {
           onChangeText={goalInputHandler}
           value={enteredGoal}
         />
-        <Button title='ADD' onPress={props.onAddGoal.bind(this, enteredGoal)} />
+        <Button title='CANCEL' color='red' />
+        <Button title='ADD' onPress={addGoalHandler} />
       </View>
     </Modal>
   )
